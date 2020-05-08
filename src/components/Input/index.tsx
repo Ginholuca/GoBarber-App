8a75f2af8d4b7ab944622898e4ce4e6678eae244
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-05-08 17:03:29 -0300

    app

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,6 @@
 import React, {
+  useState,
+  useCallback,
   useEffect,
   useRef,
   useImperativeHandle,
@@ -31,6 +33,25 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   const { registerField, defaultValue = '', fieldName, error } = useField(name)
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
 
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false)
+
+    setIsFilled(!!inputValueRef.current.value)
+
+    // if (inputValueRef.current.value) {
+    //   setIsFilled(true)
+    // } else {
+    //   setIsFilled(false)
+    // }
+  }, [])
+
   useImperativeHandle(ref, () => ({
     // colocando "({}) para não precisar do return"
     focus() {
@@ -56,14 +77,20 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
 
   return (
     <>
-      <Container>
-        <Icon name={icon} size={20} color="#666360" />
+      <Container isFocused={isFocused} isErrored={!!error}>
+        <Icon
+          name={icon}
+          size={20}
+          color={isFocused || isFilled ? '#ff9000' : '#666360'}
+        />
 
         <TextInput
           ref={inputElementRef}
           keyboardAppearance="dark"
           placeholderTextColor="#666360"
           defaultValue={defaultValue}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
           onChangeText={(value) => {
             inputValueRef.current.value = value
           }}
