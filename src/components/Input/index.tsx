88b0edf2963d6a5dcfd3620ece5ad74d5951a8ab
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-05-08 00:20:27 -0300

    app

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react'
 import { TextInputProps } from 'react-native'
+import { useField } from '@unform/core'
 
 import { Container, TextInput, Icon } from './styles'
 
@@ -8,15 +14,59 @@ interface InputProps extends TextInputProps {
   icon: string
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputValueReference {
+  value: string
+}
+
+interface InputRef {
+  focus(): void
+}
+
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref
+) => {
+  const inputElementRef = useRef<any>(null)
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name)
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
+
+  useImperativeHandle(ref, () => ({
+    // colocando "({}) para não precisar do return"
+    focus() {
+      inputElementRef.current.focus()
+    },
+  }))
+
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value
+        inputElementRef.current.setNativeProps({ text: value })
+      },
+      clearValue() {
+        inputValueRef.current.value = ''
+        inputElementRef.current.clear()
+      },
+    })
+  }, [fieldName, registerField])
+
   return (
     <>
       <Container>
         <Icon name={icon} size={20} color="#666360" />
 
         <TextInput
+          ref={inputElementRef}
           keyboardAppearance="dark"
           placeholderTextColor="#666360"
+          defaultValue={defaultValue}
+          onChangeText={(value) => {
+            inputValueRef.current.value = value
+          }}
           {...rest}
         />
       </Container>
@@ -24,4 +74,4 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
   )
 }
 
-export default Input
+export default forwardRef(Input)
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   Image,
   View,
   ScrollView,
   KeyboardAvoidingView,
   Platform,
+  TextInput,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -17,7 +20,11 @@ import logoImg from '../../assets/logo.png'
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
   const navigation = useNavigation()
+
+  const emailInputRef = useRef<TextInput>(null)
+  const passwordInputRef = useRef<TextInput>(null)
 
   return (
     <>
@@ -37,18 +44,46 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Form ref={formRef} onSubmit={() => {}}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current.focus()
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current.focus()
+                }}
+              />
 
-            <Input name="password" icon="lock" placeholder="Senha" />
+              <Input
+                ref={emailInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current?.submitForm()}
+              />
 
-            <Button
-              onPress={() => {
-                console.log('a')
-              }}
-            >
-              Entrar
-            </Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
